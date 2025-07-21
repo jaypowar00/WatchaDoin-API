c5dfe8c7811a4e3e8d2cdc2c6106fb54cea5a0e2
jaypowar00 <jaypowar00@gmail.com>
2025-07-21 23:29:45 +0530

    🕵️‍♂️ Create django app "social"

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user',
     'corsheaders',
+    'user',
+    'social'
 ]
 
 MIDDLEWARE = [
