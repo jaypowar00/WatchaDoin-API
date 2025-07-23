c206b384ab53975bf060aca410b68c3f3e7c9fb7
jaypowar00 <jaypowar00@gmail.com>
2025-07-24 01:23:48 +0530

    🐛️ Bug fix for modularized social app

--- social/urls/connections.py
+++ social/urls/connections.py
@@ -2,8 +2,8 @@ from django.urls import path
 from social.views import connections
 
 urlpatterns = [
-	path('/followings', connections.followings_list, name='followings_list'),
-	path('/followers', connections.followers_list, name='followers_list'),
-    path('/follow', connections.follow_user, name='follow_user'),
+	path('followings', connections.followings_list, name='followings_list'),
+	path('followers', connections.followers_list, name='followers_list'),
+    path('follow', connections.follow_user, name='follow_user'),
 	# path('/unfollow', views.unfollow_user, name='unfollow'),                             # TODO 1
 ]
